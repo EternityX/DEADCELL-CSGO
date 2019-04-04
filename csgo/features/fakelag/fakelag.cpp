ae060e7428171095db237867c311f814a03f42dc
TXIuTnVsbA <qqsmallyu80@gmail.com>
2019-04-04 13:35:58 +0800

    Update fakelag.cpp

--- csgo/features/fakelag/fakelag.cpp
+++ csgo/features/fakelag/fakelag.cpp
@@ -1,30 +1,29 @@
 #include "fakelag.h"
 
-void c_fakelag::think( CUserCmd* cmd ) {
+void c_fakelag::think(CUserCmd* cmd) {
 	g_cl.m_sendpacket = true;
 
 	static int choke = 0;
-	auto local = C_CSPlayer::get_local( );
-	if( g_vars.misc.fakelag.enabled && !KEYDOWN(VK_K) && !(cmd->m_buttons & IN_ATTACK)) {
-
-		switch( g_vars.misc.fakelag.type ) {
-		case 0: {// Adaptive
-			choke = std::min< int >(static_cast<int>(std::ceilf(64 / (g_cl.m_local->velocity().Length() * g_init.m_global_vars->m_interval_per_tick))), g_vars.misc.fakelag.amount);
-			break;
-		}
-		case 1:{ // Maximum
-			choke = g_vars.misc.fakelag.amount;
-			break;
+	auto local = C_CSPlayer::get_local();
+	if (g_vars.misc.fakelag.enabled && !KEYDOWN(VK_K) && !(cmd->m_buttons & IN_ATTACK)) {
+			switch (g_vars.misc.fakelag.type) {
+			case 0: { // Maximum
+				choke = std::min< int >(g_vars.misc.fakelag.amount + 1, 14);
+				break;
 			}
-		default:
-			choke = 1;
+			case 1: { // Adaptive
+				choke = std::min< int >(static_cast<int>(std::ceilf(64 / (g_cl.m_local->velocity().Length() * g_csgo.m_global_vars->m_interval_per_tick))), 14);
+				break;
+			}
+			default:
+				choke = 1;
+				break;
 		}
 	}
-	else
-	{
+	else {
 		choke = 1;
 	}
-	if ( choke > static_cast< int >( g_csgo.m_clientstate->m_nChokedCommands ) )
+	if (choke > static_cast<int>(g_csgo.m_clientstate->m_nChokedCommands))
 		g_cl.m_sendpacket = false;
 }
 
